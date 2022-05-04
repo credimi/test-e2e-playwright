867fd5d55a5ae446bde6372c18c7ea2cdcb51fd3
Andrea Bianchi <andreaiaia@users.noreply.github.com>
2022-05-04 11:55:31 +0200

    add describe

---
+++ tests/intro-step.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test.use({
+    ignoreHTTPSErrors: true
+});
+
+test.describe('Intro Step Application tests', () => {
+    test('If the user fills correctly the intro form but the VAT code is not active, then it shows a message "Partita IVA non trovata"', async ({ page }) => {
+
+        // Go to https://www.test-e2e-playwright-website.qa.credimi.com/ottieni-proposta/
+        await page.goto('https://www.test-e2e-playwright-website.qa.credimi.com/ottieni-proposta/');
+
+        // Go to https://www.test-e2e-playwright-website.qa.credimi.com/ottieni-proposta/#step-intro
+        await page.goto('https://www.test-e2e-playwright-website.qa.credimi.com/ottieni-proposta/#step-intro');
+
+        // Click text=Accetta
+        await page.locator('text=Accetta').click();
+
+        // Fill input[name="email"]
+        await page.locator('input[name="email"]').fill('test@credimi.com');
+
+        // Fill text=Partita IVAInserisci la Partita IVA dell’azienda per cui stai facendo richiesta >> [placeholder=" "]
+        await page.locator('text=Partita IVAInserisci la Partita IVA dell’azienda per cui stai facendo richiesta >> [placeholder=" "]').fill('4271597854891');
+
+        // Click text=Prosegui
+        await page.locator('text=Prosegui').click();
+
+        // Click text=Partita IVA non valida, riprova!
+        await expect(page.locator('text=Partita IVA non valida, riprova!')).toBeVisible();
+    });
+});
