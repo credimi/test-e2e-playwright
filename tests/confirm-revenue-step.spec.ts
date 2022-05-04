0e90d02582fe245e2a48949eb125cf9f09cbe0b4
Andrea Bianchi <andreaiaia@users.noreply.github.com>
2022-05-04 15:28:50 +0200

    Add test

--- tests/confirm-revenue-step.spec.ts
+++ tests/confirm-revenue-step.spec.ts
@@ -25,4 +25,32 @@ test.describe('Confirm Revenue Step Application Tests', () => {
         // Click text=Campo obbligatorio
         await expect(page.locator('text=Campo obbligatorio')).toBeVisible();
     });
+
+
+    test('3a. If the user is not elegible, then he steps on the no product page', async ({ page }) => {
+        // Go to https://www.test-20220419-website.qa.credimi.com/ottieni-proposta/
+        await page.goto('https://www.test-20220419-website.qa.credimi.com/ottieni-proposta/');
+        // Click text=Accetta
+        await page.locator('text=Accetta').click();
+        // Fill input[name="email"]
+        await page.locator('input[name="email"]').fill('test@credimi.com');
+        // Fill text=Partita IVAInserisci la Partita IVA dell’azienda per cui stai facendo richiesta >> [placeholder=" "]
+        await page.locator('text=Partita IVAInserisci la Partita IVA dell’azienda per cui stai facendo richiesta >> [placeholder=" "]').fill('00283140762');
+        // Click text=Verifica se la tua azienda è finanziabile in 1 clickEmailPartita IVAInserisci la >> svg
+        await page.locator('text=Verifica se la tua azienda è finanziabile in 1 clickEmailPartita IVAInserisci la >> svg').click();
+        // Click text=Prosegui
+        await Promise.all([
+            page.waitForNavigation(/*{ url: 'https://www.test-20220419-website.qa.credimi.com/ottieni-proposta/#step-revenue' }*/),
+            page.locator('text=Prosegui').click()
+        ]);
+        // Fill [placeholder="\36 0\.000"]
+        await page.locator('[placeholder="\\36 0\\.000"]').fill('100000');
+        // Click text=Prosegui
+        await Promise.all([
+            page.waitForNavigation(/*{ url: 'https://www.test-20220419-website.qa.credimi.com/ottieni-proposta/#step-noproducts' }*/),
+            page.locator('text=Prosegui').click()
+        ]);
+        // Click text=Ci dispiace! Non possiamo procedere con la tua richiesta
+        await expect(page.locator('text=Ci dispiace! Non possiamo procedere con la tua richiesta')).toBeVisible();
+    });
 });
